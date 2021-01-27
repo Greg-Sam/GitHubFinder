4f72246d59949e83e6f8bb3dbd220ec2c69c363d
Greg Samuelson <grssam@gmail.com>
2021-01-27 11:44:47 -0800

    through lesson 23

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Navbar from './components/layout/Navbar'
-import './App.css';
+import Users from './components/users/Users'
+import User from './components/users/User'
+import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
+import axios from 'axios'
+import About from './components/pages/About'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null
+  }
 
+  // async componentDidMount() {
+  //   this.setState({ loading: true })
+
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ users: res.data, loading: false })
+  // }
+  // search github users
+  searchUsers = async text => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false })
+  }
+
+  // get single github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false })
+  }
+
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false })
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
   render() {
-    const name = 'John Doe'
-    const loading = true
-
-
+    const { users, user, loading } = this.state
     return (
-      <nav className="navbar bg-primary">
-        <Navbar />
-      </nav>
+      <Router>
+        <div>
+          <div className="navbar bg-primary">
+            <Navbar />
+          </div>
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert} />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User { ...props } 
+                getUser={this.getUser} 
+                user={user} 
+                loading={loading} />
+              )} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 
